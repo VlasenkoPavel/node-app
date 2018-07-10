9b0e3fc6f94ab80152108821fa5b73072db7f26d
Pavel Vlasenko <psvlasenko@gmail.com>
2018-07-10 08:57:40 +0300

    minor fix

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -43,7 +43,7 @@ let InitialState: State = {
 
 const fieldNums = [3,6,9,12,15,18,21,24,27,30,33,36,2,5,8,11,14,17,20,23,26,29,32,35,1,4,7,10,13,16,19,22,25,28,31,34,0];
 const whellNums = [5,24,16,33,1,20,14,31,9,22,18,29,7,28,12,35,3,26,0,32,15,19,4,21,2,25,17,34,6,27,13,36,11,30,8,23,10];
-const blackNums =[10,24,33,20,31,22,29,28,35,26,8,11,13,,6,17,2,4,15]
+const blackNums = [10,24,33,20,31,22,29,28,35,26,8,11,13,,6,17,2,4,15]
 
 const Cell = ({ number, count, className, onClick }: cellProps) => 
     <div className={className} onClick={() => { onClick(number) }}>
