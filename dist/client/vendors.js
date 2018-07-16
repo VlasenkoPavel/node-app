fcf8a4ec18955573bb9670e76a034041e8898031
Pavel Vlasenko <psvlasenko@gmail.com>
2018-07-16 09:54:10 +0300

    ignore dist

--- dist/client/vendors.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../../public/styles/jumbotron-narrow.css");
-require("../../public/bootstrap-4.1.1/dist/css/bootstrap.css");
