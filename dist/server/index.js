7fe4638032e40a3ad91e50ae0aae32fdc30ae446
Pavel Vlasenko <psvlasenko@gmail.com>
2018-07-16 10:01:46 +0300

    fix in gitignore

--- dist/server/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-const aplication_1 = require("./aplication/core/aplication");
-const config = require('../../config');
-const app = new aplication_1.Aplication(config);
-app.start();
